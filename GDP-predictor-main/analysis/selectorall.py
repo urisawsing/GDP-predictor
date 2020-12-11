7c0a785be8fd8eb38a7cba3a51f4738309b893a2
SaioaE <75852883+SaioaE@users.noreply.github.com>
2020-12-11 17:51:27 +0100

    Add files via upload

---
+++ GDP-predictor-main/analysis/selectorall.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append("..")
+import utils
+from utils import config as cn
+from analysis import reader as re
+import pandas as pd
+import numpy as np
+from analysis import countrylist as clist
+
+def selection(numcorr):
+    data=re.readall()
+    num=clist.numadapt()
+    cat=data['CountryCode'].astype('category')
+    data['CountryCode']=cat
+    correction=data['CountryCode'].cat.codes
+    data['CountryCode']=correction
+    resized = data.pivot_table(index = ['CountryCode','Year'], columns = "IndicatorCode", values = 'Value')
+    resized['COUNTRYENC']=resized.index.get_level_values(0)
+    resized['NextYearGDP']=resized['NY.GDP.MKTP.KD.ZG'].shift(periods=-1)
+    resized=resized.drop(index=2010,level=1)
+    corr=resized.corr()
+    gdpcorr=corr['NextYearGDP']
+    absgdp=abs(gdpcorr)
+    high=absgdp.nlargest(numcorr)
+    return high
+
+def indicators(ncorr):
+    data=re.readall()
+    num=clist.numadapt()
+    cat=data['CountryCode'].astype('category')
+    data['CountryCode']=cat
+    correction=data['CountryCode'].cat.codes
+    data['CountryCode']=correction
+    resized = data.pivot_table(index = ['CountryCode','Year'], columns = "IndicatorCode", values = 'Value')
+    resized['COUNTRYENC']=resized.index.get_level_values(0)
+    resized['NextYearGDP']=resized['NY.GDP.MKTP.KD.ZG'].shift(periods=-1)
+    chosen=selection(ncorr)
+    indicators=pd.DataFrame(chosen)
+    indexs=indicators.index.values.tolist()
+    bestcorr=resized[indexs]
+    swapped=resized.swaplevel(0,1)
+    df2010=swapped.loc[[2010]]
+    resized=resized.drop(index=2010,level=1)
+    final=bestcorr.to_numpy()
+    return final, df2010
