24043538fac15b8ece7eba03275555dd89bdd291
urisawsing <72299686+urisawsing@users.noreply.github.com>
2020-12-11 19:36:35 +0100

    Add files via upload

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,13 @@
+import numpy as np
+import pandas as pd
 import sys
 sys.path.append("..")
+import sqlite3
+import analysis as an
+import os
 
-def i
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import r2_score, mean_squared_error
