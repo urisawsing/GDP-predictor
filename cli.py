9f7058eb70b07186459084be46e8b767d4b57c4a
urisawsing <72299686+urisawsing@users.noreply.github.com>
2020-12-14 23:10:10 +0100

    Add files via upload

--- cli.py
+++ cli.py
@@ -21,12 +21,54 @@ parser.add_argument(
     "task",
     choices=["train", "predict"],
     help="Task to be performed",
+parser.add_argument(
+    "model",
+    choices=["GradBoost", "MultiLin"],
+    help="Model used",
 )
+parser.add_argument(
+    "priority",
+    choices=["Time", "Results"],
+    help="Choosing between fast execution or exploratory execution",
+)
+    
 # You can add here custom optional arguments to your program
 
 if __name__ == "__main__":
     args = parser.parse_args()
     if args.task == "train":
         logging.info("Training")
+    
+        if args.model =="Gradboost":
+            logging.info("Gradient_Boosting_Method")
+    
+            if args.priority =="Time":
+                logging.info("Priorizing_Time")
+                R=models.GBmodelTrain()
+            
+            if args.priority =="Results":
+                logging.info("Priorizing_Results")
+                models.GBmodelTrain()
+                R=models.ExhaustiveGBM()
+    
+        if args.model=="MultiLin":
+            logging.info("MultiLinear_Method")
+    
+            if args.priority =="Time":
+                logging.info("Priorizing_Time")
+                R=models.multilinearTrain()
+    
+            if args.priority =="Results":
+                logging.info("Priorizing_Results")
+                R=models.ExhaustiveML()
+            
+    
+    
     if args.task == "predict":
-        logging.info("Predinting")
+        logging.info("Predicting")
+    
+        if args.model =="GradBoost":
+            models.GBmodelPredict()
+    
+        if args.model =="MultiLin":
+            models.multilinearPredict()
