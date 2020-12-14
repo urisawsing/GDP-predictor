ecdf5335e811d14be64d27711b82021d295de567
urisawsing <72299686+urisawsing@users.noreply.github.com>
2020-12-14 16:47:21 +0100

    Add files via upload

--- analysis/selectorall.py
+++ analysis/selectorall.py
@@ -17,6 +17,8 @@ def fselection(answer=False):
     correction=data['CountryCode'].cat.codes
     data['CountryCode']=correction
     resized = data.pivot_table(index = ['CountryCode','Year'], columns = "IndicatorCode", values = 'Value')
+    resized['COUNTRYENC']=resized.index.get_level_values(0)
+    resized['NextYearGDP']=resized['NY.GDP.MKTP.KD.ZG'].shift(periods=-1)
     print("Do you want to use the selection of indicators predefined?")
     de=input("In the case you want type Yes, if not type No")
     if de=="No"or de=="no" or de=="N" or de=="noup":
@@ -28,8 +30,9 @@ def fselection(answer=False):
         high.to_csv("bestindicators.csv")
     else:
         high=pd.read_csv("bestindicators.csv")
+        
     print("Indicators selected\n")
-    return high
+    return high,data,num
 
 def selection(answer=False):
     print("Getting the best indicators...")
@@ -47,10 +50,8 @@ def selection(answer=False):
 
 
 
-def indicators():
+def indicators(chosen,data,num):
     print("Obtaining the data...\n")
-    data=re.readall()
-    num=clist.numadapt()
     cat=data['CountryCode'].astype('category')
     data['CountryCode']=cat
     correction=data['CountryCode'].cat.codes
@@ -58,15 +59,12 @@ def indicators():
     resized = data.pivot_table(index = ['CountryCode','Year'], columns = "IndicatorCode", values = 'Value')
     resized['COUNTRYENC']=resized.index.get_level_values(0)
     resized['NextYearGDP']=resized['NY.GDP.MKTP.KD.ZG'].shift(periods=-1)
-    chosen=selection()
     indicators=pd.DataFrame(chosen)
     indexs=indicators['Unnamed: 0']
     indexs=indexs.to_numpy()
     indexs=list(indexs)
     bestcorr=resized.iloc[:,indexs]  
-    swapped=resized.swaplevel(0,1)
-    df2010=swapped.loc[[2010]]
-    resized=resized.drop(index=2010,level=1)
+    #resized=resized.drop(index=2010,level=1)
     final=bestcorr.to_numpy()
     print("Data obtained\n")
-    return final, df2010
+    return resized
