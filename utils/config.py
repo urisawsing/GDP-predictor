b73ff1128968a016c014363dd099b1dd8ee851bc
urisawsing <72299686+urisawsing@users.noreply.github.com>
2020-12-14 18:48:09 +0100

    Add files via upload

--- utils/config.py
+++ utils/config.py
@@ -9,11 +9,3 @@ MODELS_PATH = os.path.join(BASE_DIR, "models")
 LOGS_PATH = os.path.join(BASE_DIR, "logs")
 
 NUM_PREDICTORS=50
-
-GBM_HYP={
-        'n_estimators': 100,
-        'max_depth': 10,
-        'learning_rate': 0.1,
-        'loss': 'ls',
-       
-}
--- utils/io.py
+++ utils/io.py
@@ -1,11 +1,12 @@
 import sys
 sys.path.append("..")
-
 import utils
 from utils import config as cn
-import analysis as an
-import sqlite3
+from analysis import reader as re
 import pandas as pd
+import numpy as np
+from analysis import countrylist as clist
+from models import model_countries_together as allc
 
 #readers
 def readall():
@@ -39,9 +40,11 @@ def numadapt():
     return countries
 
 #selectors
-def selection(numcorr):
-    data=readall()
-    num=numadapt()
+
+def fselection(answer=False):
+    print("Selecting the best indicators...\n")
+    data=re.readall()
+    num=clist.numadapt()
     cat=data['CountryCode'].astype('category')
     data['CountryCode']=cat
     correction=data['CountryCode'].cat.codes
@@ -49,16 +52,39 @@ def selection(numcorr):
     resized = data.pivot_table(index = ['CountryCode','Year'], columns = "IndicatorCode", values = 'Value')
     resized['COUNTRYENC']=resized.index.get_level_values(0)
     resized['NextYearGDP']=resized['NY.GDP.MKTP.KD.ZG'].shift(periods=-1)
-    resized=resized.drop(index=2010,level=1)
-    corr=resized.corr()
-    gdpcorr=corr['NextYearGDP']
-    absgdp=abs(gdpcorr)
-    high=absgdp.nlargest(numcorr)
+    print("Do you want to use the selection of indicators predefined?")
+    de=input("In the case you want type Yes, if not type No")
+    if de=="No"or de=="no" or de=="N" or de=="noup":
+        print("This option may take a while(like 10min), please wait")
+        model=allc.GBmodelTrain(ncorr=1329)
+        a=model.feature_importances_
+        df=pd.DataFrame(a)
+        high=df[0].nlargest(50)
+        high.to_csv("bestindicators.csv")
+    else:
+        high=pd.read_csv("bestindicators.csv")
+        
+    print("Indicators selected\n")
+    return high,data,num
+
+def selection(answer=False):
+    print("Getting the best indicators...")
+    data=re.readall()
+    num=clist.numadapt()
+    cat=data['CountryCode'].astype('category')
+    data['CountryCode']=cat
+    correction=data['CountryCode'].cat.codes
+    data['CountryCode']=correction
+    resized = data.pivot_table(index = ['CountryCode','Year'], columns = "IndicatorCode", values = 'Value')
+    high=pd.read_csv("bestindicators.csv")
+    print("Done\n")
     return high
 
-def indicators(ncorr):
-    data=readall()
-    num=numadapt()
+
+
+
+def indicators(chosen,data,num):
+    print("Obtaining the data...\n")
     cat=data['CountryCode'].astype('category')
     data['CountryCode']=cat
     correction=data['CountryCode'].cat.codes
@@ -66,21 +92,12 @@ def indicators(ncorr):
     resized = data.pivot_table(index = ['CountryCode','Year'], columns = "IndicatorCode", values = 'Value')
     resized['COUNTRYENC']=resized.index.get_level_values(0)
     resized['NextYearGDP']=resized['NY.GDP.MKTP.KD.ZG'].shift(periods=-1)
-    chosen=selection(ncorr)
     indicators=pd.DataFrame(chosen)
-    indexs=indicators.index.values.tolist()
-    bestcorr=resized[indexs]
-    swapped=resized.swaplevel(0,1)
-    df2010=swapped.loc[[2010]]
-    resized=resized.drop(index=2010,level=1)
+    indexs=indicators['Unnamed: 0']
+    indexs=indexs.to_numpy()
+    indexs=list(indexs)
+    bestcorr=resized.iloc[:,indexs]  
+    #resized=resized.drop(index=2010,level=1)
     final=bestcorr.to_numpy()
-    return final, df2010
-
-
-
-#def retrieve_training_dataset():
- #   pass
-
-
-#def retrieve_predict_dataset():
- #   pass
+    print("Data obtained\n")
+    return resized
