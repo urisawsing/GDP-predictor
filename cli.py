b8d2749a1efd36f61ca496769f4147bac091ceb8
urisawsing <72299686+urisawsing@users.noreply.github.com>
2020-12-15 02:21:08 +0100

    Add files via upload

--- cli.py
+++ cli.py
@@ -9,28 +9,29 @@ sys.path.append('.')
 
 from utils import config, io, models
 
-
 logging.basicConfig(
-    filename=os.path.join(config.LOGS_PATH, datetime.now().strftime('cli_%Y-%m-%d_%H:%M:%S.log')),
-    format="%(asctime)s - [%(levelname)s] - %(message)s",
     level=logging.INFO,
+    filename=os.path.join(config.LOGS_PATH, datetime.now().strftime('cli_%Y-%m-%d_%H.%M.%S.log')),
+    format="%(asctime)s - [%(levelname)s] - %(message)s"
 )
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "task",
     choices=["train", "predict"],
-    help="Task to be performed",
+    help="Task to be performed"
 )
 parser.add_argument(
     "model",
     choices=["GB", "ML"],
-    help="Model used, gradientboost or multilinear",
+    default="GB",
+    help="Model used, gradientboost or multilinear"
 )
 parser.add_argument(
     "priority",
     choices=["T", "R"],
-    help="Choosing between fast execution(time) or exploratory execution(results)",
+    default="T",
+    help="Choosing between fast execution(time) or exploratory execution(results)"
 )
     
 # You can add here custom optional arguments to your program
@@ -48,8 +49,8 @@ if __name__ == "__main__":
                 R=models.GBmodelTrain()
             
             if args.priority =="R":
-                logging.info("Priorizing_Results")
-                models.GBmodelTrain()
+                msg="Priorizing_Results with "+str(config.EXHAUSTIVE_ITER)+" iterations"
+                logging.info(msg)
                 R=models.ExhaustiveGBM()
     
         if args.model=="ML":
@@ -60,9 +61,10 @@ if __name__ == "__main__":
                 R=models.multilinearTrain()
     
             if args.priority =="R":
-                logging.info("Priorizing_Results")
+                msg="Priorizing_Results with "+str(config.EXHAUSTIVE_ITER)+" iterations"
+                logging.info(msg)
                 R=models.ExhaustiveML()
-            
+        logging.info(R)
     
     
     if args.task == "predict":
